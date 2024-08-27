ac2166ee7b430c825c5bf7c8a3744ceabd29b170
AmyMell <melloncraft1@gmail.com>
2024-08-27 14:52:02 +0200

    27.08.2024

---
+++ kroko/kroko.js
@@ -0,0 +1,56 @@
+let firstNum = 0;
+let secondNum = 0;
+var outputDiv = document.getElementById("score");
+var score = 0;
+
+function reset() {
+  updateNumbers();
+}
+
+function updateNumbers() {
+  firstNum = getRandomNumber();
+  secondNum = getRandomNumber();
+  document.getElementById("firstNum").innerHTML = firstNum;
+  document.getElementById("secondNum").innerHTML = secondNum;
+}
+
+function getRandomNumber() {
+  return Math.round(Math.random() * 10);
+}
+
+function right() {
+  if (firstNum < secondNum) {
+    var add1 = 1;
+    score += add1;
+    outputDiv.innerHTML = score;
+    updateNumbers();
+  } else {
+    var minus1 = 1;
+    score -= minus1
+    document.getElementById("score").innerHTML = score;
+  }
+}
+function left() {
+  if (firstNum > secondNum) {
+    var add1 = 1;
+    score += add1;
+    outputDiv.innerHTML = score;
+    updateNumbers();
+  } else {
+    var minus1 = 1;
+    score -= minus1
+    document.getElementById("score").innerHTML = score;
+  }
+}
+function even() {
+  if (firstNum === secondNum) {
+    var add1 = 1;
+    score += add1;
+    outputDiv.innerHTML = score;
+    updateNumbers();
+  } else {
+    var minus1 = 1;
+    score -= minus1
+    document.getElementById("score").innerHTML = score;
+  }
+}
